1d61e009c25df96fb04e0ab614c8f936024f5339
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-12-01 00:20:01 -0300

    Updates

---
+++ precache-manifest.6236e0a94969b61f6a098e50bf5bb10a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375d6113561c08e16825c54468a34b0c",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "29f7a142511177f6454b",
+    "url": "/blog/static/css/main.5f23160c.chunk.css"
+  },
+  {
+    "revision": "58a3358c77507f089c91",
+    "url": "/blog/static/js/2.307923ab.chunk.js"
+  },
+  {
+    "revision": "29f7a142511177f6454b",
+    "url": "/blog/static/js/main.3b471519.chunk.js"
+  },
+  {
+    "revision": "a10e7ea48e8a0640f44d",
+    "url": "/blog/static/js/runtime-main.49332528.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/blog/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
